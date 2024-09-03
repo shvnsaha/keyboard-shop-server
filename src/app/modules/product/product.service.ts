3d9bf036e82bcc8ef2f0c944c548696c0d45d856
shvnsaha <shovan.css2018@gmail.com>
2024-09-04 00:36:39 +0600

    project done

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -10,47 +10,66 @@ const createProductIntoDB = async (payload: TProduct) => {
 }
 
 const getAllProductsFromDB = async (query: Record<string, unknown>) => {
-  
+
+
   const sort = query?.sort as string || '-createdAt'
   const searchTerm = query?.searchTerm as string || ''
-  const page = Number(query.page) || 1;
-  const limit = Number(query.limit) || 1;
-  const min = Number(query.min) || 0;
-  const max = Number(query.max) || 2000;
-  const skip = (page-1)*limit;
-  if(query?.id){
-   const objectIdArray = (query?.id as string).split(',').map(id => new mongoose.Types.ObjectId(id))
-   const result = await Product.find({ _id: { $in: objectIdArray } })
-   const total = await Product.countDocuments()
-   return{
-    result,
-    total
-   }
-  
-  }
-  const search = Product.find({
-    $or: [
-      { name: { $regex: new RegExp(searchTerm, 'i') } },
-      { brand: { $regex: new RegExp(searchTerm, 'i') } },
-    ],
-  })
-  const price = search.find({
-    price: { $gte: min, $lte: max },
-  })
-  const result = await price.find({ isDeleted: false }).sort(sort).skip(skip).limit(limit)
-   const total = await Product.countDocuments({
-    $or: [
-      { name: { $regex: new RegExp(searchTerm, 'i') } },
-      { brand: { $regex: new RegExp(searchTerm, 'i') } },
-    ],
-    price: { $gte: min, $lte: max },
-    isDeleted: false,
-  } )
+  const page = Number(query.page);
+  const limit = Number(query.limit);
+  const min = Number(query.min)
+  const max = Number(query.max);
+  const skip = (page - 1) * limit;
 
+  // console.log(sort, searchTerm, page, limit, min, max, skip);
+  if (query?.id) {
+    const objectIdArray = (query?.id as string).split(',').map(id => new mongoose.Types.ObjectId(id))
+    const result = await Product.find({ _id: { $in: objectIdArray } })
+    const total = await Product.countDocuments()
+    return {
+      result,
+      total
+    }
+  }
+
+
+  if (searchTerm || min || max) {
+    const search = Product.find({
+      $or: [
+        { name: { $regex: new RegExp(searchTerm, 'i') } },
+        { brand: { $regex: new RegExp(searchTerm, 'i') } },
+      ],
+    })
+    const price = search.find({
+      price: { $gte: min, $lte: max },
+    })
+
+    const result = await price.find({ isDeleted: false }).sort(sort).skip(skip).limit(limit)
+    const total = await Product.countDocuments({
+      $or: [
+        { name: { $regex: new RegExp(searchTerm, 'i') } },
+        { brand: { $regex: new RegExp(searchTerm, 'i') } },
+      ],
+      price: { $gte: min, $lte: max },
+      isDeleted: false,
+    })
+    return {
+      result, total
+    }
+
+  }
+
+
+  const result = await Product.find({ isDeleted: false }).sort('-createdAt').skip(skip).limit(limit)
+  const total =  await Product.countDocuments({isDeleted:false})
   return {
-    result,total
+    result, total
   }
 }
+
+// const getAllProductsFromDB = async(query: Record<string, unknown>)=>{
+//   console.log(query);
+//   const result = await 
+// }
 
 const getSingleProductFromDB = async (id: string) => {
   const result = await Product.findById(id)
@@ -78,6 +97,9 @@ const updateProductIntoDB = async (id: string, payload: Partial<TProduct>) => {
   })
   return result
 }
+
+
+
 
 export const ProductServices = {
   createProductIntoDB,
